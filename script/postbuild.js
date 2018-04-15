fa843272abb95f86a0dec0d35cc51022d45944e2
Stefan Schult <stefan@schult.mobi>
2018-04-15 15:41:30 +0200

    add social links

---
+++ script/postbuild.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+const { promisify } = require('util');
+
+const fs = require('fs');
+const path = require('path');
+
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+String.prototype.replaceAll = function(search, replacement) {
+  return this.replace(new RegExp(search, 'g'), replacement);
+};
+const filePath = path.normalize(`${__dirname}/../dist/index.html`);
+
+async function main() {
+  try {
+    const fileBuffer = await readFileAsync(filePath);
+    const currentUrl = process.env.URL || './';
+    const fileContent = fileBuffer
+      .toString()
+      .replaceAll('/socialsharing.', `${currentUrl}socialsharing.`)
+      .replaceAll('###URL###', `${currentUrl}`);
+    await writeFileAsync(filePath, fileContent)
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+
+main();
---
+++ script/screenshot.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+let pathFile = `file://${path.normalize(`${__dirname}/../dist/index.html#card`)}`;
+
+if(process.env.NODE_ENV === 'development') {
+  pathFile = 'http://localhost:1234/index.html#card'
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({width: 200, height: 150, deviceScaleFactor: 2.5});
+  await page.goto(pathFile);
+  await page.screenshot({path: 'app/assets/socialsharing.png'});
+  await browser.close();
+})();
