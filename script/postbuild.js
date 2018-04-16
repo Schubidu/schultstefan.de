ffcfeff3d9d1836374430d0f0ef56c60e9ae8823
Stefan Schult <stefan@schult.mobi>
2018-04-16 09:00:12 +0200

    set http2 headers

--- script/postbuild.js
+++ script/postbuild.js
@@ -10,9 +10,10 @@ const writeFileAsync = promisify(fs.writeFile);
 String.prototype.replaceAll = function(search, replacement) {
   return this.replace(new RegExp(search, 'g'), replacement);
 };
-const filePath = path.normalize(`${__dirname}/../dist/index.html`);
+const distPath = path.normalize(`${__dirname}/../dist/`);
 
-async function main() {
+async function stringReplaces() {
+  const filePath = path.normalize(`${distPath}index.html`);
   try {
     const fileBuffer = await readFileAsync(filePath);
     const url = process.env.URL + '/' || './';
@@ -23,10 +24,29 @@ async function main() {
       .replaceAll('###URL###', `${url}`)
       .replaceAll('class="hide"', `class="hide" viewBox="0 0 300 100"`)
       .replaceAll('class="i"', `class="i" viewBox="0 0 100 100"`)
-    await writeFileAsync(filePath, fileContent)
+    return await writeFileAsync(filePath, fileContent)
   } catch (err) {
     console.error('ERROR:', err);
   }
 }
 
-main();
+async function headers() {
+  const filePath = `${distPath}_headers`;
+  const jsKey = 'all.js';
+  const cssKey = 'screen.css';
+  const manifest = require(`${distPath}parcel-manifest.json`);
+  try {
+    const jsValue = manifest[jsKey];
+    const cssValue = manifest[cssKey];
+    const fileContent = `/
+  Link: ${jsValue}; rel=preload; as=script
+  Link: ${cssValue}; rel=preload; as=style`;
+
+    return await writeFileAsync(filePath, fileContent)
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+
+stringReplaces();
+headers();
