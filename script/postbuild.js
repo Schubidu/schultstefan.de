39cd2be602f46b458f3aad2419191f7f691bbd7b
Stefan Schult <stefan@schult.mobi>
2018-04-16 09:58:29 +0200

    cleanup files

--- script/postbuild.js
+++ script/postbuild.js
@@ -1,52 +1,82 @@
 /* eslint-disable no-console */
-const { promisify } = require('util');
+const { promisify } = require("util");
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
+const unlinkAsync = promisify(fs.unlink);
 
 String.prototype.replaceAll = function(search, replacement) {
-  return this.replace(new RegExp(search, 'g'), replacement);
+  return this.replace(new RegExp(search, "g"), replacement);
 };
 const distPath = path.normalize(`${__dirname}/../dist/`);
 
-async function stringReplaces() {
-  const filePath = path.normalize(`${distPath}index.html`);
-  try {
-    const fileBuffer = await readFileAsync(filePath);
-    const url = process.env.URL + '/' || './';
-    const deployUrl = process.env.DEPLOY_URL + '/' || './';
-    const fileContent = fileBuffer
-      .toString()
-      .replaceAll('/socialsharing.', `${deployUrl}socialsharing.`)
-      .replaceAll('###URL###', `${url}`)
-      .replaceAll('class="hide"', `class="hide" viewBox="0 0 300 100"`)
-      .replaceAll('class="i"', `class="i" viewBox="0 0 100 100"`)
-    return await writeFileAsync(filePath, fileContent)
-  } catch (err) {
-    console.error('ERROR:', err);
-  }
-}
+const asyncProcessor = [
+  async function stringReplaces() {
+    const filePath = path.normalize(`${distPath}index.html`);
+    try {
+      const fileBuffer = await readFileAsync(filePath);
+      const url = process.env.URL + "/" || "./";
+      const deployUrl = process.env.DEPLOY_URL + "/" || "./";
+      const fileContent = fileBuffer
+        .toString()
+        .replaceAll("/socialsharing.", `${deployUrl}socialsharing.`)
+        .replaceAll("###URL###", `${url}`)
+        .replaceAll('class="hide"', `class="hide" viewBox="0 0 300 100"`)
+        .replaceAll('class="i"', `class="i" viewBox="0 0 100 100"`);
+      await writeFileAsync(filePath, fileContent);
+    } catch (err) {
+      console.error("ERROR:", err);
+    }
+  },
 
-async function headers() {
-  const filePath = `${distPath}_headers`;
-  const jsKey = 'all.js';
-  const cssKey = 'screen.css';
-  const manifest = require(`${distPath}parcel-manifest.json`);
-  try {
-    const jsValue = manifest[jsKey];
-    const cssValue = manifest[cssKey];
-    const fileContent = `/
+  async function headers() {
+    const manifestPath = `${distPath}parcel-manifest.json`;
+    const filePath = `${distPath}_headers`;
+    const jsKey = "all.js";
+    const cssKey = "screen.css";
+    try {
+      const manifest = require(manifestPath);
+      const jsValue = manifest[jsKey];
+      const cssValue = manifest[cssKey];
+      const fileContent = `/
   Link: ${jsValue}; rel=preload; as=script
   Link: ${cssValue}; rel=preload; as=style`;
 
-    return await writeFileAsync(filePath, fileContent)
+      await writeFileAsync(filePath, fileContent);
+      return Promise.resolve({ toDelete: [manifestPath, manifestPath] });
+    } catch (err) {
+      console.error("ERROR:", err);
+    }
+  }
+];
+
+async function main() {
+  const cleanupArrays = ["toDelete"];
+  try {
+    const filesToCleanup = await Promise.all(asyncProcessor.map(fn => fn()));
+    const { toDelete } = filesToCleanup.reduce((acc, method) => {
+      let curAcc = {};
+      cleanupArrays.forEach(cleanup => {
+        if (method) {
+          if (method[cleanup]) {
+            curAcc[cleanup] = [
+              ...(acc[cleanup] || []),
+              ...method[cleanup]
+            ].filter((v, i, a) => a.indexOf(v) === i);
+          }
+        }
+      });
+      return curAcc;
+    }, {});
+
+    // delete unnecessary  files
+    await Promise.all(toDelete.map(filePath => unlinkAsync(filePath)));
   } catch (err) {
-    console.error('ERROR:', err);
+    console.error("ERROR:", err);
   }
 }
 
-stringReplaces();
-headers();
+main();
