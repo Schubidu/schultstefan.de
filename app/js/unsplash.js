147e49d07b19d54cdfa28b137bdfc94478ef7699
Stefan Schult <stefan@schult.mobi>
2018-05-05 10:56:47 +0200

    linking photo

--- app/js/unsplash.js
+++ app/js/unsplash.js
@@ -1,5 +1,6 @@
 ((doc, documentElement, body) => {
   const {
+    id,
     color,
     user: {
       name: unsplashUser,
@@ -20,7 +21,7 @@
   );
 
   const aside = doc.createElement("aside");
-  aside.innerHTML = `Photo by <a href="${unsplashProfil}?utm_source=Stefan%27s%20Photo%20App%20Example%0D%0ADemo&utm_medium=referral" rel=”nofollow”>${unsplashUser}</a> on <a href="https://unsplash.com/?utm_source=Stefan%27s%20Photo%20App%20Example%0D%0ADemo&utm_medium=referral" rel=”nofollow”>Unsplash</a>`;
+  aside.innerHTML = `<a href="https://unsplash.com/photos/${id}?utm_source=Stefan%27s%20Photo%20App%20Example%0D%0ADemo&utm_medium=referral">Photo</a> by <a href="${unsplashProfil}?utm_source=Stefan%27s%20Photo%20App%20Example%0D%0ADemo&utm_medium=referral" rel=”nofollow”>${unsplashUser}</a> on <a href="https://unsplash.com/?utm_source=Stefan%27s%20Photo%20App%20Example%0D%0ADemo&utm_medium=referral" rel=”nofollow”>Unsplash</a>`;
 
   body.append(aside);
 })(document, document.documentElement, document.body);
