af55cf6df46289230ee2a2704b4624be6b7fa38d
Stefan Schult <Schubidu@users.noreply.github.com>
2018-07-28 15:43:19 +0200

    select specific image from url (#5)

---
+++ app/js/unplashPhoto.js
@@ -0,0 +1,25 @@
+import queryParser from './queryParser';
+import { fetchImageData, getRandomImage, hasImage } from './unsplash-images';
+
+export default async () => {
+  const query = await queryParser();
+  const availePhoto = await hasImage(query.get('photos'));
+  let photo = '';
+
+  if (history.pushState) {
+    if (!availePhoto) {
+      photo = await getRandomImage();
+    } else {
+      photo = query.get('photos');
+    }
+
+    query.set('photos', photo);
+
+    const newUrl = `${window.location.protocol}//${window.location.host}${
+      window.location.pathname
+    }?${query.toString()}`;
+    window.history.pushState({ path: newUrl }, '', newUrl);
+  }
+
+  return fetchImageData(photo);
+};
