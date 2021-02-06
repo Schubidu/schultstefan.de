6563fe808fc12aadc497a283b0cad396f7f144dd
Stefan Schult <Schubidu@users.noreply.github.com>
2021-02-06 21:10:21 +0100

    enable spa in development

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: ['@snowpack/plugin-typescript'],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    { match: 'routes', src: '.*', dest: '/index.html' },
   ],
   optimize: {
     /* Example: Bundle your final build: */
