525676059d2e1a9fd99f4bea3ac2d3009c2d500e
Stefan Schult <Schubidu@users.noreply.github.com>
2021-02-21 00:09:00 +0100

    fix: get data from json

--- api/open-graph-image.ts
+++ api/open-graph-image.ts
@@ -1,16 +1,20 @@
 import type { NowRequest, NowResponse } from '@vercel/node';
 import { decode } from 'blurhash';
 import { createCanvas } from 'canvas';
-import { getRandomImageData } from '../src/unplashPhoto';
+import data from '../data.json';
 
 const viewportWidth = 1200;
 const viewPortHeight = 630;
 
+function getRandomData() {
+  return data[Math.floor(Math.random() * data.length - 1)];
+}
+
 module.exports = async (req: NowRequest, res: NowResponse) => {
   try {
-    const currentPhoto = await getRandomImageData();
+    const currentPhoto = await getRandomData();
     if (currentPhoto) {
-      const pixels = decode(currentPhoto.blurHash, 1200, 630);
+      const pixels = decode(currentPhoto.blur_hash, 1200, 630);
 
       const canvas = createCanvas(1200, 630);
       const ctx = canvas.getContext('2d');
