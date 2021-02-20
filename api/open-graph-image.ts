b606e49e99abfd39c873cd57e5a03d4a66d57a21
Stefan Schult <Schubidu@users.noreply.github.com>
2021-02-20 23:52:41 +0100

    fix: possible null

--- api/open-graph-image.ts
+++ api/open-graph-image.ts
@@ -9,17 +9,19 @@ const viewPortHeight = 630;
 module.exports = async (req: NowRequest, res: NowResponse) => {
   try {
     const currentPhoto = await getRandomImageData();
-    const pixels = decode(currentPhoto.blurHash, 1200, 630);
+    if (currentPhoto) {
+      const pixels = decode(currentPhoto.blurHash, 1200, 630);
 
-    const canvas = createCanvas(1200, 630);
-    const ctx = canvas.getContext('2d');
-    const imageData = ctx.createImageData(viewportWidth, viewPortHeight);
-    imageData.data.set(pixels);
-    ctx.putImageData(imageData, 0, 0);
-    res.setHeader('Content-type', 'image/png');
-    res.send(canvas.toBuffer('image/png'));
+      const canvas = createCanvas(1200, 630);
+      const ctx = canvas.getContext('2d');
+      const imageData = ctx.createImageData(viewportWidth, viewPortHeight);
+      imageData.data.set(pixels);
+      ctx.putImageData(imageData, 0, 0);
+      res.setHeader('Content-type', 'image/png');
+      res.send(canvas.toBuffer('image/png'));
+    }
   } catch (err) {
     console.error(err);
-    return null;
   }
+  return null;
 };
